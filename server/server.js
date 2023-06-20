3ba25ac6bc511192efccc6c566d8934818e375b4
Mayank <maychotaliya9898@gmail.com>
2023-06-20 10:55:42 +0530

    Error-7

--- server/server.js
+++ server/server.js
@@ -26,8 +26,20 @@ app.use(logger);
 app.use(credentials);
 
 // Cross Origin Resource Sharing
-app.use(cors(corsOptions));
-app.use(cors());
+// app.use(cors(corsOptions));
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://idyllic-creponne-45a6ec.netlify.app/');
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  // Pass to next layer of middleware
+  next();
+})
 
 // built-in middleware to handle urlencoded form data
 app.use(express.urlencoded({ extended: false }));
